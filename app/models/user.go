94fd590b95f6ef1672a58f929d5f572728c0be6b
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-01-16 21:26:16 -0300

    feat: user model

--- app/models/user.go
+++ app/models/user.go
@@ -1,12 +1,24 @@
 package models
 
 import (
-	"github.com/goravel/framework/database/orm"
+	"time"
 )
 
 type User struct {
-	orm.Model
-	Name   string
-	Avatar string
-	orm.SoftDeletes
+	Model
+
+	Name            string    `gorm:"column:name;not null;" json:"name,omitempty"`
+	Avatar          string    `gorm:"column:avatar;default:null" json:"avatar,omitempty"`
+	Email           string    `gorm:"column:email;not null" json:"email,omitempty"`
+	EmailVerifiedAt time.Time `gorm:"column:email_verified_at" json:"email_verified_at,omitempty"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
+
+type Session struct {
+	User        User   `json:"user"`
+	AccessToken string `json:"access_token"`
+	ExpiresAt   int64  `json:"expires_at"`
 }
