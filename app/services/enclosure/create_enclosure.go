de07cffc2501c5aa0cda014acaf83bf6c9b4bef4
Vicente <vicente19981@live.com>
2025-01-19 12:44:03 -0300

    feat: create an enclosure services

---
+++ app/services/enclosure/create_enclosure.go
@@ -0,0 +1,21 @@
+package enclosure
+
+import (
+	"github.com/goravel/framework/facades"
+	"github.com/startup-of-zero-reais/zoo-api/app/http/requests"
+	"github.com/startup-of-zero-reais/zoo-api/app/http/responses"
+	"github.com/startup-of-zero-reais/zoo-api/app/models"
+)
+
+func (e enclosureImpl) Create(er requests.CreateEnclosure) (models.Enclosure, error) {
+	var enclosure models.Enclosure
+
+	enclosure.Identification = er.Identification
+
+	err := facades.Orm().Query().Create(&enclosure)
+	if err != nil {
+		return models.Enclosure{}, responses.ErrUnhandledPgError
+	}
+
+	return enclosure, nil
+}
