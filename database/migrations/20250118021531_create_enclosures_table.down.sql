dcc8455ceecd45fec4f36b08eb940b435f3f7cef
Vicente <vicente19981@live.com>
2025-01-19 14:39:45 -0300

    fix: migrations enclosure

--- database/migrations/20250118021531_create_enclosures_table.down.sql
+++ database/migrations/20250118021531_create_enclosures_table.down.sql
@@ -1,4 +1,2 @@
 DROP TRIGGER IF EXISTS set_updated_at_enclosures ON enclosures;
-DROP FUNCTION IF EXISTS update_updated_at_column;
 DROP TABLE IF EXISTS enclosures;
-DROP TABLE IF EXISTS _timestamps;
--- database/migrations/20250118021531_create_enclosures_table.up.sql
+++ database/migrations/20250118021531_create_enclosures_table.up.sql
@@ -1,31 +1,15 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS _timestamps (
-    deleted_at TIMESTAMP WITH TIME ZONE,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-
-
 CREATE TABLE enclosures (
   id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
   identification VARCHAR(255) NOT NULL,
+  search_vector TSVECTOR GENERATED ALWAYS AS (to_tsvector('portuguese', identification)) STORED,
 
   UNIQUE (id)
 ) INHERITS (_timestamps);
 
 
-CREATE INDEX IF NOT EXISTS deletet_at_idx ON users(deleted_at) WHERE deleted_at IS NULL;
-CREATE INDEX IF NOT EXISTS idx_users_updated_at ON users(updated_at DESC);
+CREATE INDEX IF NOT EXISTS idx_enclsoure_search_vector ON enclosures USING GIN(search_vector);
+CREATE INDEX IF NOT EXISTS deletet_at_idx ON enclosures(deleted_at) WHERE deleted_at IS NULL;
+CREATE INDEX IF NOT EXISTS idx_enclosures_updated_at ON enclosures(updated_at DESC);
 
 CREATE OR REPLACE TRIGGER set_updated_at_enclosures
 BEFORE UPDATE ON enclosures FOR EACH ROW
