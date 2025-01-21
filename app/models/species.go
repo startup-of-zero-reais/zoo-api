1bb2bfa9885db938be145d5adee49518591ee21a
Vicente <vicente19981@live.com>
2025-01-20 22:42:52 -0300

    refactor: adding on requests and models new columns

--- app/models/species.go
+++ app/models/species.go
@@ -3,8 +3,10 @@ package models
 type Species struct {
 	Model
 
-	CommonName    string `gorm:"column:common_name;default:null" json:"common_name"`
-	CientificName string `gorm:"column:cientific_name;default:null" json:"cientific_name"`
+	CommonName     string `gorm:"column:common_name;default:null" json:"common_name"`
+	CientificName  string `gorm:"column:cientific_name;default:null" json:"cientific_name"`
+	Kind           string `gorm:"column:kind;default:null" json:"kind"`
+	TaxonomicOrder string `gorm:"column:taxonomic_order;default:null" json:"taxonomic_order"`
 }
 
 func (Species) TableName() string {
