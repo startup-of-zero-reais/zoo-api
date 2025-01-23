0a1384703cf592e5ebaec9f574b46d0d345ba74a
Vicente <vicente19981@live.com>
2025-01-23 20:31:51 -0300

    fix: omit empty washer and microchip

--- app/models/animal.go
+++ app/models/animal.go
@@ -5,8 +5,8 @@ import "time"
 type Animal struct {
 	Model
 	Name          string    `gorm:"column:name;default:null;" json:"name,omitempty"`
-	WasherCode    string    `gorm:"column:washer_code" json:"washer_code"`
-	MicrochipCode string    `gorm:"column:microchip_code" json:"microchip_code"`
+	WasherCode    string    `gorm:"column:washer_code;default:null" json:"washer_code,omitempty"`
+	MicrochipCode string    `gorm:"column:microchip_code;default:null" json:"microchip_code,omitempty"`
 	LandingAt     time.Time `gorm:"column:landing_at" json:"landing_at"`
 	Origin        string    `gorm:"column:origin" json:"origin"`
 	BornDate      time.Time `gorm:"column:born_date" json:"born_date"`
