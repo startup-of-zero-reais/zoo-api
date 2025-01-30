1995b239592800a8b442fa01be26edd6bcf60836
Vicente <vicente19981@live.com>
2025-01-30 00:20:03 -0300

    refactor: removing empty lines

--- app/services/upload/remove_animals.go
+++ app/services/upload/remove_animals.go
@@ -1,14 +1,11 @@
 package upload
 
 import (
-	"errors"
-
 	"github.com/goravel/framework/facades"
 	"github.com/startup-of-zero-reais/zoo-api/app/models"
 )
 
 func (e *uploadImpl) RemoveAnimals(ids []string) error {
-
 	result, err := facades.Orm().Query().Where("id IN ?", ids).Delete(&models.ImportAnimals{})
 	if err != nil {
 		facades.Log().Errorf("failed to get import animals %v", err)
@@ -16,7 +13,7 @@ func (e *uploadImpl) RemoveAnimals(ids []string) error {
 	}
 
 	if result.RowsAffected == 0 {
-		return errors.New("no records deleted")
+		facades.Log().Warningf("no rows affected on ids %v", ids)
 	}
 
 	return nil
