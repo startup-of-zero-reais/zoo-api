87d0c6ae8baa90d44733e7e9fb61ee7229a4f147
Vicente <vicente19981@live.com>
2025-02-03 21:29:24 -0300

    feat: creating helper to join with and

--- app/helpers/helpers.go
+++ app/helpers/helpers.go
@@ -1,5 +1,7 @@
 package helpers
 
+import "strings"
+
 func Filter[T any](items []T, shouldInclude func(int, T) bool) []T {
 	filtered := make([]T, 0, len(items))
 
@@ -10,4 +12,17 @@ func Filter[T any](items []T, shouldInclude func(int, T) bool) []T {
 	}
 
 	return filtered
+}
+
+func JoinWithAnd(fields []string) string {
+	switch len(fields) {
+	case 0:
+		return ""
+	case 1:
+		return fields[0]
+	case 2:
+		return fields[0] + " e " + fields[1]
+	default:
+		return strings.Join(fields[:len(fields)-1], ", ") + " e " + fields[len(fields)-1]
+	}
 }
